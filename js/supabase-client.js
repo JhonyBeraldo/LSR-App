55b2e8fb7a00146efdafcfb046ad2c1266b6fd3a
JhonyBeraldo <alquimia.indicador@gmail.com>
2026-09-22 15:57:53 -0300

    Add files via upload

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -2,8 +2,8 @@
 // TODO: substituir pelas credenciais reais do seu projeto Supabase
 // (mesmo padrão usado no Evvo / Treemali)
 
-const SUPABASE_URL = 'https://SEU-PROJETO.supabase.co';
-const SUPABASE_ANON_KEY = 'SUA_ANON_KEY_AQUI';
+const SUPABASE_URL = 'https://udzwpfzzonrmpbaxmnlx.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkendwZnp6b25ybXBiYXhtbmx4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAwOTk1MDksImV4cCI6MjEwNTY3NTUwOX0.L43gP2XHAXPFjDwnUT_cXtMoRVhNK7acUIT5EyhyWoc';
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
